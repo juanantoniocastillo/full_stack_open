663bf6787bb31d8db3e137b903fa2a6e7616b504
Juan Antonio <juanan_38@hotmail.es>
2025-06-26 15:23:29 +0200

    Part 1d - Exercise 7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,11 +20,29 @@ const Buttons = ({options, handleClick}) => (
 )
 
 const Total = ({options}) => (
-  <p>
+  <>
     {options.map((option) => (<span key={option.total_id}>{option.text} {option.state_var}<br /></span>))}
-  </p>
+  </>
 )
 
+const Average = ({clickValues}) => {
+  let sum = 0
+  clickValues.map((value) => (sum += value))
+  const average = sum / clickValues.length
+  return (
+    <>Average: {average}<br /></>
+  )
+}
+
+const Positive = ({clickValues, good}) => <>Positive: {(good/clickValues.length)*100}%<br /></>
+
+const Statistics = ({options, clickValues, good}) => (
+  <p>
+    <Total options={options} />
+    <Average clickValues={clickValues} />
+    <Positive clickValues={clickValues} good={good} />
+  </p>
+)
 
 const App = () => {
   // This application displays the total number of collected feedback for each category: good, neutral and bad.
@@ -33,14 +51,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  // event handlers
-  const handleClick = (option) => () => {
-    console.log('Pressed', option.text, 'button. Previous value:', option.state_var)
-    const updatedVar = option.state_var + 1
-    option.state_func(updatedVar)
-    console.log('New value is:', updatedVar)
-  }
+  const [clickValues, setClickvalues] = useState([])
 
   // var definition
   const header1 = 'Give feedback'
@@ -51,23 +62,37 @@ const App = () => {
       total_id: 'good_total',
       text: 'GOOD',
       state_var: good,
-      state_func: setGood
+      state_func: setGood,
+      value: 1
     },
     {
       button_id: 'neutral_button',
       total_id: 'neutral_total',
       text: 'NEUTRAL',
       state_var: neutral,
-      state_func: setNeutral
+      state_func: setNeutral,
+      value: 0
     },
     {
       button_id: 'bad_button',
       total_id: 'bad_total',
       text: 'BAD',
       state_var: bad,
-      state_func: setBad
+      state_func: setBad,
+      value: -1
     }
   ]
+
+  // event handlers
+  const handleClick = (option) => () => {
+    console.log('Pressed', option.text, 'button. Previous count:', option.state_var)
+    const updatedVar = option.state_var + 1
+    const updatedClickvalues = clickValues.concat(option.value)
+    setClickvalues(updatedClickvalues)
+    option.state_func(updatedVar)
+    console.log('New count is:', updatedVar)
+    console.log('Stored values are:', updatedClickvalues)
+  }
 
   // app body
   return (
@@ -75,7 +100,7 @@ const App = () => {
       <Header text={header1} />
       <Buttons options={feedback_options} handleClick={handleClick} />
       <Header text={header2} />
-      <Total options={feedback_options} />
+      <Statistics options={feedback_options} clickValues={clickValues} good={good}/>
     </div>
   )
 }
