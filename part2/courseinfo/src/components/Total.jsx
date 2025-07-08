657274b98133c12804e352abd0743df10f23a003
Juan Antonio <juanan_38@hotmail.es>
2025-07-08 14:37:59 +0200

    Part 2a - Exercise 3

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,5 +1,5 @@
 const Total = ({parts}) => {
-    const total = parts.reduce((sum, part) => (sum + part.exercises), 0)
+    const total = parts.reduce((sum, part) => (sum + part.exercises), 0) //Exercise 3 was already done in exercise 2
 
     return(
         <div>
