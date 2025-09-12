c6270dbf86f645fe2ea2fc35ecc186c8f527905b
Juan Antonio <juanan_38@hotmail.es>
2025-09-12 08:49:02 +0200

    Part 3a - Exercise 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,14 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  console.log(persons)
+  
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
