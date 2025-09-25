8cdfd9246eb15db472799021feb0f00c1431a85f
Juan Antonio <juanan_38@hotmail.es>
2025-09-25 05:01:34 +0200

    Part 3c - Exercise 15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -15,29 +15,6 @@ app.use(express.static('frontend/dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
-// Hardcoded database
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 // API operations
 
@@ -75,9 +52,15 @@ app.get('/api/persons/:id', (req, res) => {
 // Delete a specific resource
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-  
-  res.status(204).end()
+  Person
+    .findByIdAndDelete(id)
+    .then(deletedPerson => {
+      if (!deletedPerson) {
+        res.status(404).end()
+      } else {
+        res.status(204).end()
+      }
+    })
 })
 
 // Add a new resource
@@ -105,7 +88,6 @@ app.post('/api/persons', (req, res) => {
   newPerson
     .save()
     .then(savedPerson => {
-      console.log(savedPerson)
       res.json(savedPerson)
     })
 })
