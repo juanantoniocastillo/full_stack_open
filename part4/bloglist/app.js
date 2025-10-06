999f30aef07559bb51d69fcf68022d1187428b5a
Juan Antonio <juanan_38@hotmail.es>
2025-10-06 09:57:50 +0200

    Part 4a - Exercises 1 and 2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+// Connection to DB
+
+logger.info('Connecting to MongoDB...')
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Failed connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
