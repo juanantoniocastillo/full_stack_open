90a02a252275bb24f986033d36d79e6c3b55b1fe
Juan Antonio <juanan_38@hotmail.es>
2025-10-23 09:52:07 +0200

    Part 4b - Exercise 12

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -20,60 +20,95 @@ test('blogs are returned in JSON format', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('all blogs', () => {
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  test('are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('has id property and not _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    for (const blog of response.body){
+      assert.strictEqual(Object.hasOwn(blog, 'id'), true)
+      assert.strictEqual(Object.hasOwn(blog, '_id'), false)
+    }
+  })
 })
 
-test('all blogs has id property and not _id', async () => {
-  const response = await api.get('/api/blogs')
+describe('a request', () => {
 
-  for (const blog of response.body){
-    assert.strictEqual(Object.hasOwn(blog, 'id'), true)
-    assert.strictEqual(Object.hasOwn(blog, '_id'), false)
-  }
-})
+  test('of a new valid blog is correctly added', async () => {
+    const newBlog = {
+      title: 'El blog de Juanan',
+      author: 'Juan Antonio Castillo',
+      url: 'http://elblogdejuananinventado.com',
+      likes: 6,
+    }
 
-test('a new valid blog can be added', async () => {
-  const newBlog = {
-    title: 'El blog de Juanan',
-    author: 'Juan Antonio Castillo',
-    url: 'http://elblogdejuananinventado.com',
-    likes: 6,
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blogsAtTheEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtTheEnd.length,   helper.initialBlogs.length + 1)
 
-  const blogsAtTheEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtTheEnd.length,   helper.initialBlogs.length + 1)
+    const titles = blogsAtTheEnd.map(n => n.title)
+    assert(titles.includes('El blog de Juanan'))
+  })
 
-  const titles = blogsAtTheEnd.map(n => n.title)
-  assert(titles.includes('El blog de Juanan'))
-})
+  test('without likes property is assigned to 0', async () => {
+    const newBlog = {
+      title: 'El blog de Juanan',
+      author: 'Juan Antonio Castillo',
+      url: 'http://elblogdejuananinventado.com',
+    }
 
-test('a request without likes property is assigned to 0', async () => {
-  const newBlog = {
-    title: 'El blog de Juanan',
-    author: 'Juan Antonio Castillo',
-    url: 'http://elblogdejuananinventado.com',
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blogsAtTheEnd = await helper.blogsInDb()
+    const addedBlog = blogsAtTheEnd.find(blog => blog.title === 'El blog de Juanan')
 
-  const blogsAtTheEnd = await helper.blogsInDb()
-  const addedBlog = blogsAtTheEnd.find(blog => blog.title === 'El blog de Juanan')
+    assert.strictEqual(Object.hasOwn(addedBlog, 'likes'), true)
+    assert.strictEqual(addedBlog.likes, 0)
 
-  assert.strictEqual(Object.hasOwn(addedBlog, 'likes'), true)
-  assert.strictEqual(addedBlog.likes, 0)
+  })
+
+  test('without title property is answered with 400 Bad Request', async () => {
+    const newBlog = {
+      author: 'Juan Antonio Castillo',
+      url: 'http://elblogdejuananinventado.com',
+      likes: 6,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  })
+
+  test('without url property is answered with 400 Bad Request', async () => {
+    const newBlog = {
+      title: 'El blog de Juanan',
+      author: 'Juan Antonio Castillo',
+      likes: 6,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  })
 
 })
 
