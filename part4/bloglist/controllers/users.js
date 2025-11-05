d39800b787137f30061960cef3ce27febf373462
Juan Antonio <juanan_38@hotmail.es>
2025-11-05 06:36:32 +0100

    Part 4d - Exercise 17

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (req, res) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
   res.json(users)
 })
 
