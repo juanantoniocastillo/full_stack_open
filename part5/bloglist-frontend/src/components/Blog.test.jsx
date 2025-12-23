aaa0115ee10afdfaa5f1afa73f760a531be3e0d3
Juan Antonio <juanan_38@hotmail.es>
2025-12-23 10:49:57 +0100

    Part 5c - Exercise 15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
 
 describe('The component displaying a blog', () => {
   beforeEach(() => {
@@ -67,4 +68,56 @@ describe('The component displaying a blog', () => {
     const likesElement = screen.getByText('2')
     expect(likesElement).toBeVisible()
   })
+})
+
+test('If like button is clicked twice, the event handler recieves two calls', async () => {
+  vi.mock('../services/blogs')
+
+  const blogs = [
+    {
+      'title': 'Blog de Juanan',
+      'author': 'Juanan',
+      'url': 'www.juanan.com',
+      'likes': 2,
+      'user': {
+        'username': 'juanan38',
+        'name': 'Juan Antonio',
+        'id': '6908827eac076f59efa3436e'
+      },
+      'id': '690ae1850a61f04d428d27a6'
+    },
+    {
+      'title': 'Blog de Irene',
+      'author': 'Irene',
+      'url': 'www.irene.com',
+      'likes': 7,
+      'user': {
+        'username': 'juanan38',
+        'name': 'Juan Antonio',
+        'id': '6908827eac076f59efa3436e'
+      },
+      'id': '690ae21a236b9b71797f4afc'
+    },
+  ]
+
+  const blog = blogs[0]
+
+  blogService.updateLikes.mockResolvedValue({ ...blog, likes: blog.likes + 1 })
+  const mockSetBlogs = vi.fn()
+
+  const blogUser = {
+    username: 'juanan38'
+  }
+
+  render(<Blog blog={blog} blogs={blogs} setBlogs={mockSetBlogs} user={blogUser} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const likeButton = screen.getByText('Like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockSetBlogs.mock.calls).toHaveLength(2)
 })
