f6a917a620de130e1cf279f5a27ae1fca9178e76
Juan Antonio <juanan_38@hotmail.es>
2026-01-15 16:24:32 +0100

    Part 5d - Exercise 17

---
+++ part5/bloglist-testing/tests/bloglist.spec.js
@@ -0,0 +1,22 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        "username": "admin",
+        "name": "Administrator",
+        "password": "cambiar"
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown by default', async ({ page }) => {
+    await expect(page.getByLabel('username')).toBeVisible()
+    await expect(page.getByLabel('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
