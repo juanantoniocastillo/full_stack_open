32812443c6d36267e9408675cb42a3f981efd4a3
Juan Antonio <juanan_38@hotmail.es>
2026-01-20 14:51:18 +0100

    Part 5d - Exercise 19

--- part5/bloglist-testing/tests/bloglist.spec.js
+++ part5/bloglist-testing/tests/bloglist.spec.js
@@ -1,15 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
+
+const data = {
+  "username": "admin",
+  "name": "Administrator",
+  "password": "cambiar"
+}
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('/api/testing/reset')
-    await request.post('/api/users', {
-      data: {
-        "username": "admin",
-        "name": "Administrator",
-        "password": "cambiar"
-      }
-    })
+    await request.post('/api/users', { data })
 
     await page.goto('/')
   })
@@ -22,22 +23,35 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('admin')
-      await page.getByLabel('password').fill('cambiar')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, data['username'], data['password'])
 
       await expect(page.getByText('Administrator is logged in.')).toBeVisible()
       await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('admin')
-      await page.getByLabel('password').fill('wrong')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, data['username'], 'wrong')
 
       await expect(page.getByText('Wrong credentials.')).toBeVisible()
       await expect(page.getByText('Administrator is logged in.')).not.toBeVisible()
       await expect(page.getByRole('button', { name: 'Log out' })).not.toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, data['username'], data['password'])
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'New Blog' }).click()
+
+      await page.getByLabel('title').fill('Blog de prueba')
+      await page.getByLabel('author').fill('Er Juanan')
+      await page.getByLabel('url').fill('www.test.com')
+      await page.getByRole('button', { name: 'Create' }).click()
+      
+      await expect(page.locator('div[datatype-testid="titleNdAuthor"]').getByText('Blog de prueba')).toBeVisible()
     })
   })
 })
