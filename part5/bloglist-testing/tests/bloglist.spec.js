b96849bd978c8a2adbb056780be79d0fd5461b77
Juan Antonio <juanan_38@hotmail.es>
2026-01-21 11:41:12 +0100

    Part 5d - Exercises 22

--- part5/bloglist-testing/tests/bloglist.spec.js
+++ part5/bloglist-testing/tests/bloglist.spec.js
@@ -1,16 +1,22 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 const { loginWith, postBlog } = require('./helper')
 
-const data = {
+const user1 = {
   "username": "admin",
   "name": "Administrator",
   "password": "cambiar"
 }
 
+const user2 = {
+  "username": "second",
+  "name": "Second User",
+  "password": "pass"
+}
+
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('/api/testing/reset')
-    await request.post('/api/users', { data })
+    await request.post('/api/users', { data: user1 })
 
     await page.goto('/')
   })
@@ -23,14 +29,14 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await loginWith(page, data['username'], data['password'])
+      await loginWith(page, user1['username'], user1['password'])
 
       await expect(page.getByText('Administrator is logged in.')).toBeVisible()
       await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await loginWith(page, data['username'], 'wrong')
+      await loginWith(page, user1['username'], 'wrong')
 
       await expect(page.getByText('Wrong credentials.')).toBeVisible()
       await expect(page.getByText('Administrator is logged in.')).not.toBeVisible()
@@ -40,7 +46,7 @@ describe('Blog app', () => {
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      await loginWith(page, data['username'], data['password'])
+      await loginWith(page, user1['username'], user1['password'])
     })
 
     test('a new blog can be created', async ({ page }) => {
@@ -52,7 +58,7 @@ describe('Blog app', () => {
 
   describe('When there is one blog', () => {
     beforeEach(async ({ page }) => {
-      await loginWith(page, data['username'], data['password'])
+      await loginWith(page, user1['username'], user1['password'])
       await postBlog(page, 'Blog de prueba', 'Er Juanan', 'www.test.com')
       await page.getByRole('button', { name: 'view' }).click()
     })
@@ -73,6 +79,17 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'Remove' }).click()
 
       await expect(page.locator('div[datatype-testid="titleNdAuthor"]').getByText('Blog de prueba')).not.toBeVisible()
+    })
+
+    test('it can not be deleted by a user who does not added it', async ({ page, request }) => {
+      await request.post('/api/users', { data: user2 })
+      await page.getByRole('button', { name: 'Log out' }).click()
+
+      await loginWith(page, user2['username'], user2['password'])
+      await expect(page.getByText('Second User is logged in.')).toBeVisible()
+
+      await page.getByRole('button', { name: 'view' }).click()
+      await expect(page.getByRole('button', { name: 'Remove' })).not.toBeVisible()
     })
   })
 })
